28e1f872b14335800494be4b3ce8f6e4fc80e0e2
Enzo Cazenave <encaze24@hotmail.com>
2023-06-15 12:10:58 -0300

    Add changes

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -3,7 +3,11 @@ import styles from '../styles/components/Chart.module.css'
 export const Chart = () => {
 	return (
 		<section className={styles.container}>
-
+			<h2>Estado de carrera universitaria</h2>
+			<div className={styles.chart}>
+				<span className={styles.chartPercentage}>65%</span>
+			</div>
+			<span className={styles.chartSubjects}>11/50 materias</span>
 		</section>
 	)
 }
